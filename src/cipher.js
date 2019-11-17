a00bc6bf7350e3d526e5b3625484ac43d817f812
vanesa <vanesa.gonzalez395@gmail.com>
2019-11-17 15:49:46 -0300

    Se cambió html y css. Se agregaron nuevan funciones y se crearon casos de prubas para esas funciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,49 +1,166 @@
 window.cipher = {
-  encode: (string, number) => {
-    
-    let stringCode = '';
- 
-    for (let i=0; i<string.length; i++){
 
-        let upperCaseLetter = string.toUpperCase();
-        let asciiPosition= upperCaseLetter.charCodeAt(i);
-        let newAsciiPosition;
-
-        if (asciiPosition >= 65 && asciiPosition <=90){
-            newAsciiPosition = (asciiPosition - 65 + parseInt(number)) % 26 + 65;
-            stringCode += String.fromCharCode(newAsciiPosition);
-        }else if (asciiPosition === 32){
-            stringCode += ' ';
+    newCharCodeAt: (letter,status=0) => {
+        const specialChar = ['á','é','í','ó','ú','Á','É','Í','Ó','Ú','¿','ñ','Ñ'];
+        const myAsciiChar = [200,201,202,203,204,205,206,207,208,209,210,211,212]
+        let result = '';
+        if ( status ) {
+            const indexChar = myAsciiChar.indexOf(letter);
+            result = specialChar[indexChar]
         }else{
-            break;
+            const indexAscii = specialChar.indexOf(letter);
+            result = myAsciiChar[indexAscii]
         }
-    }
-   
-    //output.innerHTML = stringCode;
-    return stringCode;
-  
-  },
-  decode: (string, number) => {
+        return result;
+    },
 
-      let stringDecode = ''; 
-  
-      for (let i=0; i<string.length; i++){
-  
-          let upperCaseLetter = string.toUpperCase();
-          let asciiPosition= upperCaseLetter.charCodeAt(i);
-          let newAsciiPosition;
-  
-          if (asciiPosition >= 65 && asciiPosition <=90){
-              newAsciiPosition = (asciiPosition - 90 - parseInt(number)) % 26 + 90;
-              stringDecode += String.fromCharCode(newAsciiPosition);
-          }else if (asciiPosition === 32){
-              stringDecode += ' ';
-          }else{
-              break;
-          }
-      }
-     
-     //output.innerHTML = stringDecode;
-     return stringDecode;
-  } 
+    /*
+    * @asciiPosition (number) - obtiene el número de la posición en ascii
+    * @range (number) - rango del bloque le letras en ascii
+    * @number (number) - número de desplazamiento
+    * @mod (number) - módulo 
+    * return (string) La letra cifrada
+    */
+    operationEncodeCipher: (asciiPosition, range, number, mod=26) => {
+        let result='';
+        if(parseInt(number)>0){
+            result = (asciiPosition - range + parseInt(number)) % mod + range;
+        }else{
+           result = (asciiPosition - range - parseInt(number)) % mod + range;
+        }
+        return String.fromCharCode(result)
+    },
+
+    operationDecodeCipher:(asciiPosition, range, number, mod=26) => {
+        let result;
+        if(parseInt(number)>0){
+            result = (asciiPosition - range - parseInt(number)) % mod + range;
+        }else{
+            result = (asciiPosition - range + parseInt(number)) % mod + range;
+        }
+        return String.fromCharCode(result)
+    },
+
+    //-----------------------------------CIFRAR--------------------------------------------
+
+    encode:(string,number)=>{
+    
+        let stringCode = '';
+ 
+        for (let i=0; i<string.length; i++){
+
+            const specialEncodeChar = ['á','é','í','ó','ú','Á','É','Í','Ó','Ú','¿','ñ','Ñ'];
+            let asciiPosition= string.charCodeAt(i);
+            //let newAsciiPosition;
+        
+            // -------------Cifra letras mayúsculas -----------------
+
+            if (asciiPosition >= 65 && asciiPosition <=90){
+                stringCode += cipher.operationEncodeCipher(asciiPosition, 65, number);
+            }
+        
+            // ------------Cifra letras minúsculas ------------------
+
+            if (asciiPosition >= 97 && asciiPosition <= 122){
+                stringCode += cipher.operationEncodeCipher(asciiPosition, 97, number);
+            }
+        
+            // ------------Cifra caracteres especiales ---------------
+
+            if (asciiPosition >= 33 && asciiPosition <= 64){
+                stringCode += cipher.operationEncodeCipher(asciiPosition, 33, number, 32);
+            } 
+
+            // ------------Agrega espacios ----------------------------
+        
+            if ( asciiPosition === 32) {
+                stringCode += ' ';
+            }
+
+            // -----------Cifrar letras con acento -------------------------
+
+            for (let j=0; j<specialEncodeChar.length; j++){
+            
+                let newSpecialPosition;
+
+                if( string[i] === specialEncodeChar[j] ){
+
+                    let specialAsciiChar = cipher.newCharCodeAt(specialEncodeChar[j],0);
+                    if (specialAsciiChar >= 200 && specialAsciiChar <= 212){     
+                        if (parseInt(number) > 0){  
+                            newSpecialPosition = (specialAsciiChar - 200 + parseInt(number)) % 13 + 200;
+                            stringCode += cipher.newCharCodeAt(newSpecialPosition,1);
+                        }else{
+                            newSpecialPosition = (specialAsciiChar - 200 - parseInt(number)) % 13 + 200;
+                            stringCode += cipher.newCharCodeAt(newSpecialPosition,1);
+                        }
+                    }
+                }
+            }
+        }
+   
+        return stringCode;
+    },
+
+    //----------------------------------DESCIFRAR---------------------------------------
+
+    decode: (string, number)=>{
+
+        let stringDecode = ''; 
+
+        for (let i=0; i<string.length; i++){
+
+            const specialDecodeChar = ['á','é','í','ó','ú','Á','É','Í','Ó','Ú','¿','ñ','Ñ'];
+            let asciiPosition= string.charCodeAt(i);
+            //let newAsciiPosition;
+
+            //-------------------Descifrar letras mayúsculas-----------------------
+
+            if (asciiPosition >= 65 && asciiPosition <=90){
+                stringDecode += cipher.operationDecodeCipher(asciiPosition, 90, number);
+            }
+
+            //------------------Descifrar letras minúsculas------------------------
+
+            if (asciiPosition >= 97 && asciiPosition <= 122){
+                stringDecode += cipher.operationDecodeCipher(asciiPosition, 122, number);
+            }
+
+            //-----------------Descifrar caracteres especiales------------------------
+
+            if (asciiPosition >= 33 && asciiPosition <= 64){
+                stringDecode += cipher.operationDecodeCipher(asciiPosition, 64, number,32);
+            }
+
+            //-----------------Agregar espacios-------------------------------------
+        
+            if ( asciiPosition === 32) {
+                stringDecode += ' ';
+            }
+
+            //-----------------Descifrar letras con acento---------------------------
+
+            for (let j=0; j<specialDecodeChar.length; j++){
+            
+                let newSpecialPosition;
+
+                if( string[i] === specialDecodeChar[j] ){
+
+                    let specialAsciiChar = cipher.newCharCodeAt(specialDecodeChar[j],0);
+                    if (specialAsciiChar >= 200 && specialAsciiChar <= 212){
+                        if(parseInt(number)>0){
+                            newSpecialPosition = (specialAsciiChar - 212 - parseInt(number)) % 13 + 212;
+                            stringDecode += cipher.newCharCodeAt(newSpecialPosition,1);
+                        }else{
+                            newSpecialPosition = (specialAsciiChar - 212 + parseInt(number)) % 13 + 212;
+                            stringDecode += cipher.newCharCodeAt(newSpecialPosition,1);
+                        }
+                    }
+                }
+            }
+        }
+   
+        return stringDecode;
+    }
+
 }
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -4,18 +4,140 @@ describe('cipher', () => {
     assert.equal(typeof cipher, 'object');
   });
 
+  //---------------------------newCharCodeAt-----------------------------
+
+  describe('cipher.newCharCodeAt',()=>{
+    
+    it('debería ser una función',()=>{
+      assert.equal(typeof cipher.newCharCodeAt, 'function');
+    });
+    
+    it('debería retornar "á" para "200" con status 1',() =>{
+      assert.equal(cipher.newCharCodeAt(200,1),'á');
+    });
+    
+    it('debería retornar "200" para "á" con status 0',() =>{
+      assert.equal(cipher.newCharCodeAt('á',0),'200');
+    });
+
+  });
+
+  //--------------------------operationEncodeCipher---------------------------------
+
+  describe('cipher.operationEncodeCipher',()=>{
+
+    it('debería ser función',()=>{
+      assert.equal(typeof cipher.operationEncodeCipher,'function');
+    });
+    
+    it('debería retornar "H" para "A" con offset 33',()=>{
+      assert.equal(cipher.operationEncodeCipher(65,65,33,26),'H');
+    });
+
+    it('debería retornar "h" para "a" con offset 33', ()=>{
+      assert.equal(cipher.operationEncodeCipher(97,97,33,26),'h');
+    });
+
+    it('debería retornar """ para "!" con offset 33',()=>{
+      assert.equal(cipher.operationEncodeCipher(33,33,33,32),'"');
+    });
+
+    it('debería retornar "H" para "A" con offset -33',()=>{
+      assert.equal(cipher.operationEncodeCipher(65,65,-33,26),'H');
+    });
+
+    it('debería retornar "h" para "a" con offset -33', ()=>{
+      assert.equal(cipher.operationEncodeCipher(97,97,-33,26),'h');
+    });
+
+    it('debería retornar """ para "!" con offset -33',()=>{
+      assert.equal(cipher.operationEncodeCipher(33,33,-33,32),'"');
+    });
+
+  });
+
+  //-------------------------------operationDecodeCipher--------------------------
+
+  describe('cipher.operationDecodeCipher',()=>{
+    
+    it('debería ser una función',()=>{
+      assert.equal(typeof cipher.operationDecodeCipher,'function');
+    });
+  
+    it('debería retornar "A" para "H" con offset 33',()=>{
+      assert.equal(cipher.operationDecodeCipher(72,90,33,26),'A');
+    });
+    
+    it('debería retornar "a" para "h" con offset 33',()=>{
+      assert.equal(cipher.operationDecodeCipher(104,122,33,26),'a');
+    });
+    
+    it('debería retornar "!" para """ con offset 33',()=>{
+      assert.equal(cipher.operationDecodeCipher(34,64,33,32),'!');
+    });
+
+    it('debería retornar "A" para "H" con offset 33',()=>{
+      assert.equal(cipher.operationDecodeCipher(72,90,-33,26),'A');
+    });
+
+    it('debería retornar "a" para "h" con offset 33',()=>{
+      assert.equal(cipher.operationDecodeCipher(104,122,-33,26),'a');
+    });
+
+    it('debería retornar "!" para """ con offset 33',()=>{
+      assert.equal(cipher.operationDecodeCipher(34,64,-33,32),'!');
+    });
+
+  });
+
+  //-------------------------------------encode------------------------------------
+  
   describe('cipher.encode', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
-
+    
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
       assert.equal(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ',33), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
-   });
+    });
+
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset -33', () => {
+      assert.equal(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ',-33), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
+    });
     
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33',()=>{
+      assert.equal(cipher.encode('abcdefghijklmnopqrstuvwxyz',33),'hijklmnopqrstuvwxyzabcdefg');
+    });
+
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset -33',()=>{
+      assert.equal(cipher.encode('abcdefghijklmnopqrstuvwxyz',-33),'hijklmnopqrstuvwxyzabcdefg');
+    });
+    
+    it('debería retornar "$" para "#" con offset 33',()=>{
+      assert.equal(cipher.encode('#',33),'$');
+    });
+    
+    it('debería retornar "$" para "#" con offset -33',()=>{
+      assert.equal(cipher.encode('#',-33),'$');
+    });
+    
+    it('debería retornar "ÍÓÚ¿ñÑáéíóúÁÉ" para "áéíóúÁÉÍÓÚ¿ñÑ" con offset 33',()=>{
+      assert.equal(cipher.encode('áéíóúÁÉÍÓÚ¿ñÑ',33),'ÍÓÚ¿ñÑáéíóúÁÉ');
+    });
+   
+    it('debería retornar "ÍÓÚ¿ñÑáéíóúÁÉ" para "áéíóúÁÉÍÓÚ¿ñÑ" con offset -33',()=>{
+      assert.equal(cipher.encode('áéíóúÁÉÍÓÚ¿ñÑ',-33),'ÍÓÚ¿ñÑáéíóúÁÉ');
+    });
+    
+    it('debería retornar " " para " " con   offset 33', () => {
+      assert.equal(cipher.encode(' ',33), ' ');
+    });
+ 
   });
 
+  //---------------------------------decode---------------------------------------
+ 
   describe('cipher.decode', () => {
 
     it('debería ser una función', () => {
@@ -24,7 +146,41 @@ describe('cipher', () => {
 
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
       assert.equal(cipher.decode('HIJKLMNOPQRSTUVWXYZABCDEFG',33),'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  );
-   });
+    });
+
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset -33', () => {
+      assert.equal(cipher.decode('HIJKLMNOPQRSTUVWXYZABCDEFG',-33), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+  
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33',()=>{
+        assert.equal(cipher.decode('hijklmnopqrstuvwxyzabcdefg',33),'abcdefghijklmnopqrstuvwxyz');
+    });
+  
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset -33',()=>{
+      assert.equal(cipher.decode('hijklmnopqrstuvwxyzabcdefg',-33),'abcdefghijklmnopqrstuvwxyz');
+    });
+    
+    it('debería retornar "#" para "$" con offset 33',()=>{
+      assert.equal(cipher.decode('$',33),'#');
+    });
+    
+    it('debería retornar "#" para "$" con offset -33',()=>{
+      assert.equal(cipher.decode('$',-33),'#');
+    });
+  
+    it('debería retornar "áéíóúÁÉÍÓÚ¿ñÑ" para "ÍÓÚ¿ñÑáéíóúÁÉ" con offset 33',()=>{
+      assert.equal(cipher.decode('ÍÓÚ¿ñÑáéíóúÁÉ',33),'áéíóúÁÉÍÓÚ¿ñÑ');
+    });
+  
+    it('debería retornar "áéíóúÁÉÍÓÚ¿ñÑ" para "ÍÓÚ¿ñÑáéíóúÁÉ" con offset -33',()=>{
+      assert.equal(cipher.decode('ÍÓÚ¿ñÑáéíóúÁÉ',-33),'áéíóúÁÉÍÓÚ¿ñÑ');
+    });
+
+    it('debería retornar " " para " " con offset 33', () => {
+      assert.equal(cipher.decode(' ',33),' '  );
+    });
 
   });
+  
 });
+
